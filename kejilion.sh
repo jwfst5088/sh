15460bf6bf5c0d020ade8e81b445238869978553
jwfst5088 <138176547+jwfst5088@users.noreply.github.com>
2023-10-04 16:20:38 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -1561,9 +1561,9 @@ case $choice in
       clear
       # 安装 X-UI
       #sudo apt update
-      #sudo apt install snapd nginx
-      #sudo snap install core
-      #sudo snap refresh core
+      sudo apt install snapd nginx
+      sudo snap install core
+      sudo snap refresh core
       sudo snap install --classic certbot
       sudo ln -s /snap/bin/certbot /usr/bin/certbot
       
